b38b79e7617887e14162233e95e131826cd3a873
arifislamarif344 <arifislamarif344@gmail.com>
2022-09-30 10:45:19 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,54 @@
+$(document).ready(function () {
+  // Waypoints
+  //////////////////////////////////////////
+  $(".js__navigation").waypoint(
+    function (direction) {
+      if (direction == "down") {
+        $(".js__navigation").addClass("navigation__sticky");
+      } else {
+        $(".js__navigation").removeClass("navigation__sticky");
+      }
+    },
+    {
+      offset: "-100px",
+    }
+  );
+
+  // owl-carousel
+  // //////////////////////////////////////
+
+  $(".fleet-carousel").owlCarousel({
+    autoplay: true,
+    dots: true,
+    loop: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      768: {
+        items: 2,
+      },
+      900: {
+        items: 3,
+      },
+    },
+  });
+
+  // isotop
+  //////////////////////////////
+  let $btns = $(".projects__contant__button__group li");
+
+  $btns.click(function (e) {
+    $(".projects__contant__button__group li").removeClass("active");
+    e.target.classList.add("active");
+
+    let selector = $(e.target).attr("data-filter");
+    $(".grid").isotope({
+      filter: selector,
+    });
+
+    return false;
+  });
+
+  $(".projects__contant__button__group #btn1").trigger("click");
+});
